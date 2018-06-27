fec9bcc3aa0a58c51f175a04a752b1ebb65e115b
ojaswa1942 <ojaswa1942@gmail.com>
2018-06-27 20:08:22 +0530

    Bg

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
 import ImageForm from './Components/ImageForm/ImageForm'
+import Rank from './Components/Rank/Rank'
+import Particles from 'react-particles-js';
+
+const ParticlesProps = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 830
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App"> 
+      <div className="App">
+         <Particles 
+              params={ParticlesProps}
+              className='particles' /> 
         <Navigation />
         <Logo />
+        <Rank />
         <ImageForm />
      {/*<FaceRecognition />*/}
       </div>
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Rank.css'
+
+const Rank = () =>{
+	return(
+		<div className="">
+			<div className="white f3">
+				{'Ojaswa, your current rank is..'}
+			</div>
+			<div className="white f1">
+				{'#5'}
+			</div>						
+		</div>		
+	);
+}
+export default Rank;
