0fed126f313e10fe9afe0a23d2cf0141f4a60a7c
ojaswa1942 <ojaswa1942@gmail.com>
2018-06-27 16:49:21 +0530

    Added tachyons for css

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navigation = () =>{
+	return(
+		<nav style ={{display:'flex', justifyContent:'flex-end'}}>
+			<p>Sign Out</p>
+		</nav>
+		);
+}
+export default Navigation;
