a205ac0bb95c3a7b091cc5b3d108ddd2e12c81b9
ojaswa1942 <ojaswa1942@gmail.com>
2018-06-27 17:53:20 +0530

    Logoing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation'
+import Logo from './Components/Logo/Logo'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App"> 
         <Navigation />
-        {/*<Logo />
-                <ImageForm />
+        <Logo />
+{/*                <ImageForm />
                 <FaceRecognition />*/}
       </div>
     );
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () =>{
 	return(
 		<nav style ={{display:'flex', justifyContent:'flex-end'}}>
-			<p>Sign Out</p>
+			<p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
 		</nav>
 		);
 }
