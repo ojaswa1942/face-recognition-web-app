eab87983bd84b7311bd61544933e98af0fefd75d
ojaswa1942 <ojaswa1942@gmail.com>
2018-06-27 19:22:15 +0530

    ImageForm

--- src/Components/ImageForm/ImageForm.js
+++ src/Components/ImageForm/ImageForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import Tilt from 'react-tilt'
+import './ImageForm.css'
 
 const ImageForm = () =>{
 	return(
-		<div className="ma4 mt0">
-
+		<div className="">
+			<p className="f3">
+				{'This Magic Brain will detect faces in your pictures. Give it a try'}
+			</p>
+			<div className='center'>
+				<div className='form gradient-pattern center pa4 br3 shadow-2 w-80'>
+					<input type='text' className='f4 pa2 w-70 center' />
+					<button className='w-30 grow f4 link ph3 pv2 dib white bg-orange'>Detect</button>
+				</div>
+			</div>
 		</div>		
 	);
 }
