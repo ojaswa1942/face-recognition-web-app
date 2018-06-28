3b02b8d4f312dc2f0f51bcb8ea48efb3971bad49
ojaswa1942 <ojaswa1942@gmail.com>
2018-06-28 16:29:03 +0530

    Started integrating API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Logo from './Components/Logo/Logo'
 import ImageForm from './Components/ImageForm/ImageForm'
 import Rank from './Components/Rank/Rank'
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+ apiKey: 'b605f9a3a598410db5130036282301a9'
+});
 
 const ParticlesProps = {
   particles: {
@@ -28,12 +33,20 @@ class App extends Component {
   }
 
   onInput = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value})
+    console.log(this.state.input);
   }
 
   onClick = () =>{
     console.log("Click");
-    
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
 
   render() {
