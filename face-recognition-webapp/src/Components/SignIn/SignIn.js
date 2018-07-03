11cf2d5ecbc566bd131a0f8754bd4000d4a4f891
ojaswa1942 <ojaswa1942@gmail.com>
2018-07-04 01:23:08 +0530

    done

--- face-recognition-webapp/src/Components/SignIn/SignIn.js
+++ face-recognition-webapp/src/Components/SignIn/SignIn.js
@@ -29,7 +29,6 @@ class SignIn extends React.Component{
 		})
 		.then(response => response.json())
 		.then(user => {
-			console.log(user);
 			if(user.id){
 				this.props.loadUser(user);
 				this.props.routeChange('home');
