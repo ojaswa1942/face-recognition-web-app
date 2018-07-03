ddc33177040cff2ca46803bf3a25d7f93918ea81
ojaswa1942 <ojaswa1942@gmail.com>
2018-07-04 01:02:58 +0530

    Secured password and completed sign in

--- face-recognition-webapp-api/server.js
+++ face-recognition-webapp-api/server.js
@@ -19,27 +19,6 @@ const app=express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-	users: [
-	{
-		id: '123',
-		name: 'John',
-		email: 'john@gmail.com',
-		password: 'cookies',
-		entries: 0,
-		joined: new Date()
-	},	
-	{
-		id: '124',
-		name: 'Johny',
-		email: 'johny@gmail.com',
-		password: 'cookie',
-		entries: 0,
-		joined: new Date()
-	}
-	]
-}
-
 app.get('/', (req,res)=>{
 	res.send(database.users);
 })
@@ -74,11 +53,28 @@ app.post('/register', (req,res) =>{
 })
 
 app.post('/signin', (req,res)=>{
-	if(req.body.email === database.users[0].email && 
-		req.body.password === database.users[0].password )
-		return res.json(database.users[0]);
-	else
-		res.status(400).json('Failure')	
+	const {email, password} = req.body;
+	// bcrypt.compare(password, hash, function(err, res) {
+   
+	// });
+	db.select('email', 'hash').from('login')
+	.where({email})
+	.then(data => {
+		bcrypt.compare(password, data[0].hash, function(err, result) {
+			if(result)
+				{
+					return db.select('*').from('users')
+					.where('email', '=', email)
+					.then(user =>{
+						res.json(user[0])
+					})
+					.catch(err => res.status(400).json('Invalid User'))
+				}
+			else
+				return res.status(400).json("Invalid Credentials");
+		 })
+	})
+	.catch(err=> res.status(400).json('Invalid Credentials'))	
 })
 
 app.get('/profile/:id', (req,res)=>{
